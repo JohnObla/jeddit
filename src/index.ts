d677b01c35c783f2915d85cec5894af45720b1b7
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-19 22:11:59 +0000

    Created server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
 import microConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up();
 
-  //   const post = orm.em.create(Post, { title: 'my first post' });
-  //   await orm.em.persistAndFlush(post);
+  const app = express();
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  app.get('/', (_, res) => {
+    res.send('hello');
+  });
+
+  app.listen(3000, () => {
+    console.log(`Server started on localhost${3000}`);
+  });
+  process.on('SIGINT', () => {
+    console.log('Bye bye!');
+    process.exit();
+  });
 };
 
 main();
+
+process.on('SIGINT', () => {
+  console.log('Bye bye!!');
+  process.exit();
+});
