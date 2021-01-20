9cc0e828d3e4eba14ae7133dbaa774749a3ccb4c
John Obla <45395888+johnobla@users.noreply.github.comjohn@obla.com>
2021-01-20 17:37:10 +0000

    Added hello resolver, need to fix post

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
+import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
+import { PostResolver } from './resolvers/post';
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
@@ -9,9 +14,15 @@ const main = async () => {
 
   const app = express();
 
-  app.get('/', (_, res) => {
-    res.send('hello');
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
   });
+
+  apolloServer.applyMiddleware({ app });
 
   app.listen(3000, () => {
     console.log(`Server started on localhost${3000}`);
